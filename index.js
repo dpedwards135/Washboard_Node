6c806f8de2f8ab769687e1c70448833064cd633f
David Edwards <dpedwards135@gmail.com>
2018-08-16 22:55:53 -0600

    Notifications sent properly

--- index.js
+++ index.js
@@ -5,11 +5,65 @@ var stripe = require("stripe")("sk_test_9YgUPJIi44UxQ9Rr3JqQ7I20");
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+exports.sendMessageToUser = functions.https.onRequest((req, res) => {
+    const customerId = req.query.customerId;
+    console.log("Customer Id is: " + customerId);
+    const title = req.query.title;
+    console.log("Title is " + title);
+    const body = req.query.body;
+    console.log("Body is " + body);
+
+    sendMessage(customerId, title, body);
+
+    res.send("sendMessage Requested");
+    return
+});
+
+function sendMessage(customerId, title, body) {
+    // Get the Messaging service for the default app
+    var defaultMessaging = admin.messaging();
+
+    var db = admin.database();
+    var ref = db.ref('user/' + customerId);
+    ref.once("value", function (snapshot) {
+        console.log("Send Message to: " + snapshot.val());
+        var loggedIn = snapshot.child('loggedIn').val();
+        var registrationToken = snapshot.child('firebaseToken').val();
+
+        if(!loggedIn) {
+            createErrorTicket("Message Send Fail", "User " + customerId + " is not logged in");
+            return
+        } else {
+    // See documentation on defining a message payload.
+        var message = {
+            notification: {
+                title: title,
+                body: body
+              },
+            token: registrationToken
+        };
+
+        // Send a message to the device corresponding to the provided
+        // registration token.
+        admin.messaging().send(message)
+            .then((response) => {
+                // Response is a message ID string.
+                console.log('Successfully sent message:', response);
+            })
+            .catch((error) => {
+                console.log('Error sending message:', error);
+            });
+        }
+    });
+    
+}
+
 exports.printSomething = functions.database
     .ref('user')
     .onWrite((event) => {
         console.log("Something Changed")
     });
+
 
 exports.createCard = functions.database.ref('user/{userID}/cards/{cardID}').onCreate((snap, context) => {
     console.log("Create Card");
@@ -115,8 +169,8 @@ exports.createOrderInstances = functions.https.onRequest((req, res) => {
                 };
 
                 newRef.set(orderInstance);
-                
-                ref.child("instance_index/" + id).set(null);
+
+                ref.child(child.key + "/customer_instance_index/" + id).set(1);
             }
         });
 
@@ -133,6 +187,13 @@ exports.watchStatus = functions.database.ref('order_instances/{instanceID}/statu
     const instanceId = snap.after.ref.parent.key;
     db.ref('order_instances/' + instanceId + '/status_history').push().set(status); //Track all status changes
 
+    var providerStatus = 1;
+    if (status > 29) {
+        providerStatus = 0;
+    }
+    console.log("will updateProviderStatus");
+    updateProviderStatus(providerStatus, instanceId);
+
     switch (status) {
         case 0: //New Instance, needs authorization
             console.log("Status 0");
@@ -147,14 +208,15 @@ exports.watchStatus = functions.database.ref('order_instances/{instanceID}/statu
                 if (snapshot.val() != null && snapshot.val() != "") { }
                 var instanceId = snapshot.child('id').val();
                 var customerId = snapshot.child('customerId').val();
+                console.log("Charge card from no show status watcher");
                 chargeCard(19, customerId, instanceId);
                 updateProviderStatus(1, instanceId);
             });
-            
+
             break;
         case 20: //Order is picked up, awaiting return, charge customer
-            
-            
+
+
 
 
             parentRef.once("value", function (snapshot) {
@@ -162,28 +224,29 @@ exports.watchStatus = functions.database.ref('order_instances/{instanceID}/statu
                 if (snapshot.val() != null && snapshot.val() != "") { }
                 var instanceId = snapshot.child('id').val();
                 var customerId = snapshot.child('customerId').val();
+                console.log("Charge card from pickup status watcher");
                 chargeCard(20, customerId, instanceId);
             });
             break;
         case 21: //Successful charge, do nothing
             break;
         case 30: //Bags are returned, Finalize order, add payout to provider account
-            
-            if(snap.before.val() == 22) {
+
+            if (snap.before.val() == 22 || snap.before.val() == 20) {
                 db.ref('order_instances/' + instanceId + '/status').set(32);
             } else {
                 db.ref('order_instances/' + instanceId + '/status').set(41);
             }
-            
+
             break;
         case 41:
             //Do nothing, use end of period review to finalize orders
             break;
-        case (50<status<60):
+        case (50 < status < 60):
             //This is a finalized order. Do nothing. Maybe archive someday in the future to optimize database.
             break;
         default: //Any other status at any point will need to be handled by customer service until automated
-            triggerCustomerServiceTicket();
+            createErrorTicket("Status: " + status, "Instance: " + instanceId);
             break;
     }
 
@@ -251,54 +314,60 @@ Finalized (all customer service and payment issues resolved or written off):
 
 function chargeCard(status, customerId, instanceId) {
 
+    console.log("chargeCard 1");
     var db = admin.database();
     var userRef = db.ref("/user/" + customerId);
 
     // 1. Get StripeId
     userRef.once("value", function (snapshot) {
+        console.log("chargeCard 2");
         console.log(snapshot.val());
         if (snapshot.val() != null && snapshot.val() != "") {
             var stripeId = snapshot.child("stripeId").val();
-    // 2. Get price from zip code
+            // 2. Get price from zip code
             var priceRef = db.ref("/zip/" + snapshot.child("order").child("zip").val());
             priceRef.once("value", function (snapshot) {
+                console.log("chargeCard 3");
                 var taxPercent = snapshot.child("taxPercentage").val();
                 var basePrice = snapshot.child("standard_price").val();
                 var noShowFee = snapshot.child("no_show_fee").val();
-    // 3. Get number of bags from order instance
+                // 3. Get number of bags from order instance
                 var bagRef = db.ref("order_instances/{instanceID}/bags");
                 bagRef.once("value", function (bagSnap) {
+                    console.log("chargeCard 4");
                     var noShowCharge = 0;
                     var bagCount = 0;
-                    if(status == 19) {
+                    if (status == 19) {
                         console.log("No bags, no show");
                         noShowCharge = noShowFee;
                     } else {
-                    
-                    bagSnap.forEach(function (child) {
-                        bagCount = bagCount + 1;
-                    }) }
-    // 4. Calculate final price
+
+                        bagSnap.forEach(function (child) {
+                            bagCount = bagCount + 1;
+                        })
+                    }
+                    // 4. Calculate final price
                     var subTotal = noShowCharge + (basePrice * bagCount);
                     var taxes = subTotal * taxPercent;
                     var finalPrice = subTotal + taxes;
                     const map = {
-                        subTotal : subTotal,
-                        taxes : taxes,
-                        finalPrice : finalPrice,
+                        subTotal: subTotal,
+                        taxes: taxes,
+                        finalPrice: finalPrice,
                     }
                     db.ref("order_instances/" + instanceId + "/charge").set(map);
-    // 5. Charge the card
+                    // 5. Charge the card
                     stripe.charges.create({
                         amount: finalPrice,
                         currency: 'usd',
                         customer: stripeId
 
                     }, function (err, charge) {
+                        console.log("chargeCard 5");
                         if (err != null) {
                             console.log("Error: " + JSON.stringify(err));
                             db.ref('order_instances/' + instanceId + '/status').set(22);
-    // To do: Save new status and save the final price as a breakdown for receipt
+                            // To do: Save new status and save the final price as a breakdown for receipt
                         }
 
                         if (charge != null) {
@@ -320,70 +389,71 @@ function chargeCard(status, customerId, instanceId) {
 };
 
 function updateProviderStatus(providerStatus, instanceId) { //ProviderStatus 1 = open, 0 = closed
+    console.log("updateProviderStatus: " + providerStatus + " " + instanceId);
     var db = admin.database();
     var instanceRef = db.ref("order_instances/" + instanceId);
-                instanceRef.once("value", function (snap) {
-                    console.log("updateProviderStatus");
-                    const orderInstance = snap.val();
-                    if(orderInstance.providerId != null && orderInstance.providerId != "") {
-                        var providerRef = db.ref("user/" + orderInstance.providerId + "/orderInstanceIndex/" + instanceId);
-                        providerRef.set(providerStatus)
-                    }
-                });
+    instanceRef.once("value", function (snap) {
+        console.log("updateProviderStatus 2");
+        const orderInstance = snap.val();
+        if (orderInstance.providerId != null && orderInstance.providerId != "") {
+            var providerRef = db.ref("user/" + orderInstance.providerId + "/orderInstanceIndex/" + instanceId);
+            providerRef.set(providerStatus)
+        }
+    });
 }
 
 function assignToProvider(instanceId) {
     var db = admin.database();
     var instanceRef = db.ref("order_instances/" + instanceId);
-                instanceRef.once("value", function (snap) {
-                    const orderInstance = snap.val();
-                    if(orderInstance.providerId != null && orderInstance.providerId != "") {
-                        unassignFromProvider(orderInstance.providerId);
-                    }
+    instanceRef.once("value", function (snap) {
+        const orderInstance = snap.val();
+        if (orderInstance.providerId != null && orderInstance.providerId != "") {
+            unassignFromProvider(orderInstance.providerId);
+        }
 
-                    const order = snap.child("order").val();
-                    providerId = "H5WgSA2etdZiki8pSHEnYcgCUFS2"; //Later add alorithm to find best
-                    const providerIdRef = db.ref("order_instances/" + instanceId + "/providerId");
-                    providerIdRef.set(providerId);
-                    console.log("AssignedProviderRef 0");
-                    const assignedProviderRef = db.ref("user/" + providerId + "/orderInstanceIndex/" + instanceId);
-                    console.log("AssignedProviderRef 0.5");
-                    assignedProviderRef.set("1"); 
-                    console.log("AssignedProviderRef 1");
+        const order = snap.child("order").val();
+        providerId = "aYDXz0mBaDf9t0R0Zg4V4e5lYHR2"; //Later add alorithm to find best
+        const providerIdRef = db.ref("order_instances/" + instanceId + "/providerId");
+        providerIdRef.set(providerId);
+        console.log("AssignedProviderRef 0");
+        const assignedProviderRef = db.ref("user/" + providerId + "/orderInstanceIndex/" + instanceId);
+        console.log("AssignedProviderRef 0.5");
+        assignedProviderRef.set(1);
+        console.log("AssignedProviderRef 1");
 
-                    const checkProviderRef = db.ref("user/" + providerId + "/orderInstanceIndex/");
-                    
-                    checkProviderRef.once("value", function (snap1) {
-                        console.log("AssignedProviderRef 2");
-                        if(snap1.hasChild(instanceId)) {
-                            console.log("AssignedProviderRef 3");
-                            db.ref('order_instances/' + instanceId + '/status').set(10);
-                            return
-                        } else {
-                            console.log("AssignedProviderRef 4");
-                            db.ref('order_instances/' + instanceId + '/status').set(05);  
-                            return                          
-                        }
-                    });
-                });
+        const checkProviderRef = db.ref("user/" + providerId + "/orderInstanceIndex/");
+
+        checkProviderRef.once("value", function (snap1) {
+            console.log("AssignedProviderRef 2");
+            if (snap1.hasChild(instanceId)) {
+                console.log("AssignedProviderRef 3");
+                db.ref('order_instances/' + instanceId + '/status').set(10);
+                return
+            } else {
+                console.log("AssignedProviderRef 4");
+                db.ref('order_instances/' + instanceId + '/status').set(05);
+                return
+            }
+        });
+    });
 }
 
 function unassignFromProvider(providerId, instanceId) {
     var db = admin.database();
-    var providerIndexRef = db.ref('user/' + providerId + '/orderInstanceIndex/') 
+    var providerIndexRef = db.ref('user/' + providerId + '/orderInstanceIndex/')
     providerIndexRef.child(instanceId).remove();
 
     providerIndexRef.once("value", function (snap) {
         const indexList = snap.val();
-        if(indexList.hasChild(instanceId)) {
+        if (indexList.hasChild(instanceId)) {
             createErrorTicket("System", "Order instance " + instanceId +
-             " was not successfully unassigned from " + providerId);
+                " was not successfully unassigned from " + providerId);
         }
     });
 }
 
 function createErrorTicket(type, message) {
-    console.log("Error Ticket: \nType: " + type  + "\nMessage: " + message);
+    console.log("Error Ticket: \nType: " + type + "\nMessage: " + message);
 }
 
 exports.finalizeSuccessfulOrderInstances = functions.https.onRequest((req, res) => {
@@ -401,14 +471,14 @@ exports.finalizeSuccessfulOrderInstances = functions.https.onRequest((req, res) 
             var status = child.child("status").val()
             if (status == 41) {
                 ref('order_instances/' + instanceId + '/status').set(51);
-            } else if(40 < status < 50) {
+            } else if (40 < status < 50) {
                 createErrorTicket("Finalization Error", "Instance " + instanceId + " cannot be finalized. Status is: " + status);
             }
-        
+
         });
 
     });
-    
+
 
 
 });
